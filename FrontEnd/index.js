eb328fdda92952789ab04f3fc1c33b0050983b7a
long bin <david.lb90@gmail.com>
2023-04-21 17:04:47 +0200

    refactoring du code ok. inclure les eventlistner dans une boucle foreach, creation de fonction filterworks et showfilterworks. tout fonctionnel pour l'instant

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -13,8 +13,13 @@ const btnAppartmentWorks = document.getElementById("appartmentWorks");
 //Pointer le bouton filtre hotel
 const btnHotelWorks = document.getElementById("hotelWorks");
 
+//Pointer tout les filtres
+const boutonFiltre = document.querySelectorAll(".btn-filtre");
+
 // creation d'un Array pour y stocker les donnés du fetch
 let workData = [];
+let workToDisplay = [];
+let selectedCategory = 0;
 
 // creation de la fonction getWork qui ira chercher les données dans l'API. Et nous stock ces données sous forme de JSON dans le tableau work data
 const getWorks = async () => {
@@ -25,7 +30,7 @@ const getWorks = async () => {
 
 // creation de la fonction showWork pour injecter dans le DOM les données fetché par la fonction getWork. Utilisation de la fonction map pour parcourir les résultats stocker dans la array workData et j'utilise les littéraux de gabarit ${} pour concaténer mon code HTML dans le innerHtml.
 
-const showWork = async () => {
+const showWorks = async () => {
   await getWorks();
   sectionGallery.innerHTML = workData
     .map(
@@ -39,86 +44,35 @@ const showWork = async () => {
     .join("");
 };
 
-showWork();
+showWorks();
 
-const getObjectWork = async () => {
-  await getWorks();
-  for (let i = workData.length - 1; i >= 0; i--) {
-    if (workData[i].category.id != 1) {
-      workData.splice(i, 1);
-    }
+const filterWorks = (selectedCategory) => {
+  if (selectedCategory === 0) {
+    workToDisplay = workData;
+    console.log("reussi");
+  } else {
+    workToDisplay = workData.filter(function (workData) {
+      return workData.categoryId === selectedCategory;
+    });
   }
 };
-const showObjectWork = async () => {
-  await getObjectWork();
-  sectionGallery.innerHTML = workData
+
+const showFilterWorks = () => {
+  sectionGallery.innerHTML = workToDisplay
     .map(
-      (work) => ` 
+      (workToDisplay) => ` 
     <figure>
-    <img src= ${work.imageUrl} alt="${work.title}">
-    <figcaption>${work.title}</figcaption>
+    <img src= ${workToDisplay.imageUrl} alt="${workToDisplay.title}">
+    <figcaption>${workToDisplay.title}</figcaption>
     </figure>
   `
     )
     .join("");
 };
 
-const getAppartmentWork = async () => {
-  await getWorks();
-  for (let i = workData.length - 1; i >= 0; i--) {
-    if (workData[i].category.id != 2) {
-      workData.splice(i, 1);
-    }
-  }
-};
-const showAppartmentWork = async () => {
-  await getAppartmentWork();
-  sectionGallery.innerHTML = workData
-    .map(
-      (work) => ` 
-      <figure>
-      <img src= ${work.imageUrl} alt="${work.title}">
-      <figcaption>${work.title}</figcaption>
-      </figure>
-    `
-    )
-    .join("");
-};
-
-const getHotelWork = async () => {
-  await getWorks();
-  for (let i = workData.length - 1; i >= 0; i--) {
-    if (workData[i].category.id != 3) {
-      workData.splice(i, 1);
-    }
-  }
-};
-const showHotelWork = async () => {
-  await getHotelWork();
-  sectionGallery.innerHTML = workData
-    .map(
-      (work) => ` 
-        <figure>
-        <img src= ${work.imageUrl} alt="${work.title}">
-        <figcaption>${work.title}</figcaption>
-        </figure>
-      `
-    )
-    .join("");
-};
-
-btnAllWorks.addEventListener("click", function () {
-  showWork();
-});
-
-btnObjectWorks.addEventListener("click", function () {
-  showObjectWork();
-});
-
-btnAppartmentWorks.addEventListener("click", function () {
-  showAppartmentWork();
-});
-
-btnHotelWorks.addEventListener("click", function () {
-  showHotelWork();
+boutonFiltre.forEach(function (button, index) {
+  button.addEventListener("click", function () {
+    filterWorks(index);
+    showFilterWorks();
+  });
 });
