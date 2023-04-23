15519887f4d562f1eabbe503dfb0a51c207c05d4
long bin <david.lb90@gmail.com>
2023-04-23 14:22:29 +0200

    recuperation Token ok. Condition établi pour echanger de page quand token valide et enregistré le token dans le local sotorage

--- FrontEnd/assets/scripts/login.js
+++ FrontEnd/assets/scripts/login.js
@@ -1,14 +1,38 @@
-const login = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    email: "sophie.bluel@test.tld",
-    password: "S0phie",
-  }),
+let formLogin = document.getElementById("formLogin");
+let token = "";
+const test = async () => {
+  let userMail = document.getElementById("email").value;
+  let userPassword = document.getElementById("password").value;
+  console.log(userMail + "   " + userPassword);
+  let loginParam = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: userMail,
+      password: userPassword,
+    }),
+  };
+  await fetch("http://localhost:5678/api/users/login", loginParam)
+    .then((res) => res.json())
+    .then((data) => (token = data.token));
+
+  console.log(token);
+  if (token) {
+    localStorage.setItem("token", token);
+    //window.location.replace("./index.html");
+    console.log("token retrouvé");
+  } else {
+    alert("Identifiant ou mot de passe incorrect! ");
+  }
 };
 
-fetch("http://localhost:5678/api/users/login", login)
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+const userLogin = () => {
+  formLogin.addEventListener("submit", function (e) {
+    e.preventDefault();
+    test();
+  });
+};
+
+userLogin();
